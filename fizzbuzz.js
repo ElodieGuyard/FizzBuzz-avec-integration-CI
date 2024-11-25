b9ce7b1bc09cdbb3347b290af5b510b34cf279c4
Elodie Guyard <elodieg29@gmail.com>
2024-11-25 14:30:50 +0100

    Condition added for fizzbuzz return

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,14 @@
 const fizzbuzz = (number) => {
+    if (number % 3 == 0 && number % 5 == 0){
+      return "Fizzbuzz"
+    }
     if (number % 3 == 0) {
       return "Fizz"
     }
     if (number === 5) {
       return "Buzz"
     }
+
     return number
   }
   
